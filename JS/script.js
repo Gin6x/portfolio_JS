0f1af062fd0556af5533c48c26270236cd80701c
Gene Chan <ccl0413gene6@gmail.com>
2023-09-11 20:10:22 +0100

    added mockup image of ios project and tested links on topBar works

---
+++ JS/script.js
@@ -0,0 +1,16 @@
+//navBar links
+let projectsLink = document.getElementById("projects");
+let aboutLink = document.getElementById("aboutme");
+let contactLink = document.getElementById("contact");
+
+projectsLink.addEventListener('click', () =>  {
+    console.log("Projects link pressed")
+});
+
+aboutLink.addEventListener('click', () =>  {
+    console.log("About me link pressed")
+});
+
+contactLink.addEventListener('click', () =>  {
+    console.log("Contact me link pressed")
+});
