a30e41c54e1298fb5c369ee0f51010b4a0609edd
Gene Chan <ccl0413gene6@gmail.com>
2023-09-14 01:00:43 +0100

    added basic footer design

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,9 @@
 let projectsLink = document.getElementById("projects");
 let aboutLink = document.getElementById("aboutme");
 let contactLink = document.getElementById("contact");
+let iOrderforUsButton = document.getElementById("iOrderforUsButton");
+var year = document.getElementById("year");
+var currentYear = new Date().getFullYear();
 
 projectsLink.addEventListener('click', () =>  {
     console.log("Projects link pressed")
@@ -14,3 +17,10 @@ aboutLink.addEventListener('click', () =>  {
 contactLink.addEventListener('click', () =>  {
     console.log("Contact me link pressed")
 });
+
+iOrderforUsButton.addEventListener('click', () =>  {
+    console.log("Learn more of i_OrderforUs")
+});
+
+//set current year for footer
+year.innerText = currentYear;
