e56bb3deff2e459c3c6b4bdd84341ac5a822dc34
Gene Chan <ccl0413gene6@gmail.com>
2023-10-18 01:34:36 +0100

    added new page for f1_Kit ios app. basic web function now online, including mail composer calling

--- JS/contact.js
+++ JS/contact.js
@@ -3,8 +3,6 @@ let homePageLink = document.getElementById("homePage");
 let projectsLink = document.getElementById("projects");
 let aboutLink = document.getElementById("aboutme");
 let contactLink = document.getElementById("contact");
-let iOrderforUsButton = document.getElementById("iOrderforUsButton");
-let f1KitButton = document.getElementById("f1KitButton");
 var year = document.getElementById("year");
 var currentYear = new Date().getFullYear();
 
@@ -19,26 +17,13 @@ projectsLink.addEventListener('click', () =>  {
 });
 
 aboutLink.addEventListener('click', () =>  {
-    const targetSection = document.getElementById("aboutMeSection");
-    console.log("Learn more about me")
-    targetSection.scrollIntoView({ behavior: "smooth" });
+    const mainPage = "index.html"
+    window.location.href = mainPage;
 });
 
 contactLink.addEventListener('click', () =>  {
     const contactPage = "contact.html"
     window.location.href = contactPage;
-});
-
-iOrderforUsButton.addEventListener('click', () =>  {
-    console.log("Learn more of i_OrderforUs")
-    const iOrderforUsPage = "iOrderforUs.html";
-    window.location.href = iOrderforUsPage;
-});
-
-f1KitButton.addEventListener('click', () =>  {
-    console.log("Learn more of f1_Kit")
-    const f1KitPage = "f1Kit.html";
-    window.location.href = f1KitPage;
 });
 
 //set current year for footer
