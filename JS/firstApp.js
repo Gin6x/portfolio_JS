e99c005683c3438fad826a5fd950a5ead6204dd5
Gene Chan <ccl0413gene6@gmail.com>
2023-10-19 16:42:28 +0100

    added github button for apps page

--- JS/firstApp.js
+++ JS/firstApp.js
@@ -3,6 +3,7 @@ let homePageLink = document.getElementById("homePage");
 let projectsLink = document.getElementById("projects");
 let aboutLink = document.getElementById("aboutme");
 let contactLink = document.getElementById("contact");
+let githubButton = document.getElementById("firstAppGithubButton")
 var year = document.getElementById("year");
 var currentYear = new Date().getFullYear();
 
@@ -24,6 +25,12 @@ aboutLink.addEventListener('click', () =>  {
 contactLink.addEventListener('click', () =>  {
     const contactPage = "contact.html"
     window.location.href = contactPage;
+});
+
+githubButton.addEventListener('click', () =>  {
+    var link = document.getElementById("firstAppGithubLink");
+            var url = link.href;
+            var newTab = window.open(url, "_blank");
 });
 
 //set current year for footer
--- JS/secondApp.js
+++ JS/secondApp.js
@@ -5,6 +5,7 @@ let aboutLink = document.getElementById("aboutme");
 let contactLink = document.getElementById("contact");
 var year = document.getElementById("year");
 var currentYear = new Date().getFullYear();
+let githubButton = document.getElementById("secondAppGithubButton")
 
 homePageLink.addEventListener('click', () =>  {
     const homePage = "/index.html"
@@ -24,6 +25,12 @@ aboutLink.addEventListener('click', () =>  {
 contactLink.addEventListener('click', () =>  {
     const contactPage = "contact.html"
     window.location.href = contactPage;
+});
+
+githubButton.addEventListener('click', () =>  {
+    var link = document.getElementById("secondAppGithubLink");
+            var url = link.href;
+            var newTab = window.open(url, "_blank");
 });
 
 //set current year for footer
